26679f53889bb71bbc69c910349506a320a9a8df
Yegor <yegor02092002@gmail.com>
2022-09-20 19:33:42 +0300

    Current stage of app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'task';
+  listVersions = {       
+    angular: ['1.1.1', '1.2.1', '1.3.3'],
+    react: ['2.1.2', '3.2.4', '4.3.1'],
+    vue: ['3.3.1', '5.2.1', '5.1.3'],
+  }
+
+  hobbiesList: string[] = [
+    'Hiking', 
+    'Biking', 
+    'Gym', 
+    'Photography', 
+    'Swimming', 
+    'Books'
+  ];
+  
+  minDate = new Date(new Date().getFullYear() - 115, 0, 1);
+  maxDate = new Date(new Date().getFullYear() + 1, 11, 31);
+
+  registerForm = new FormGroup({
+    firstName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(15),
+    ]),
+    lastName: new FormControl('',[
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+    ]),
+    dateOfBirth: new FormControl('', [
+      Validators.required,
+    ]),
+    framework: new FormControl('', [
+      Validators.required,
+    ]),
+    frameworkVersion: new FormControl('', [
+      Validators.required,
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+    ]),
+    hobbies: new FormControl('', [
+      Validators.required
+    ]),
+  })
+
+  onSubmit(){
+    console.log(this.registerForm.value);
+    console.log(this.registerForm.controls.framework.value);
+  }
 }
